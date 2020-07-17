7aaaf7726a3be3352a7b64ad2b146151f4f8ac9f
Zheleztsova Irina <irina.z98@mail.ru>
2020-07-17 12:45:25 +0300

    map comfort

--- map.py
+++ map.py
@@ -1,41 +1,32 @@
 import requests, sys, os
 from PIL import Image
 
-
-def coordinates(x, y):
-    lat = x
-    lon = y
-    zoom = 16
+def MapParams(lat, lon,z):
+    lat = lat
+    lon = lon
+    zoom = z
     type = "map"
-    return (type, zoom, lat, lon)
+    return (str(lon) + "," + str(lat),zoom,type)
 
-
-def ll(lat, lon):
-    return str(lon) + "," + str(lat)
-
-def load_map(mp):
-    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=ll(mp[2],mp[3]), z=mp[1], type=mp[0])
+def comf_map(mp):
+    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=mp[0], z=mp[1], type=mp[2])
     response = requests.get(map_request)
     map_file = "map.jpg"
     with open(map_file, "wb") as file:
             file.write(response.content)
-    return map_file
+    im = Image.open('map.jpg').convert('RGB')
+    water = 0
+    vegetation = 0
+    k = 0
+    for pixel in im.getdata():
+        k += 1
+        # print(pixel)
+        if pixel == (184, 223, 245):
+            water += 1
+        elif pixel == (215, 242, 194) or pixel == (216, 242, 194) or pixel == (233, 248, 221):
+            vegetation += 1
 
-mp = coordinates(56.302358, 43.905130)
-load_map(mp)
+    #print('water=', water, ', vegetation=', vegetation, k)
+    return water/k,vegetation/k
 
-im = Image.open('map.jpg').convert('RGB')
-
-water = 0
-vegetation = 0
-
-k = 0
-for pixel in im.getdata():
-    k += 1
-    # print(pixel)
-    if pixel == (184, 223, 245):
-        water += 1
-    elif pixel == (215, 242, 194) or pixel == (216, 242, 194) or pixel == (233, 248, 221):
-        vegetation += 1
-
-print('water=', water / k, ', vegetation=', vegetation / k)
+comf_map(mp=MapParams(56.326210,44.079732,16))
